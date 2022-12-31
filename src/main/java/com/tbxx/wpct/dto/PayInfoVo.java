94d7969aa9d35cb8726fd3a8a5d93b0d0ef509f3
aykls <1781954235@qq.com>
2022-12-31 10:18:44 +0800

    Default Changelist

--- src/main/java/com/tbxx/wpct/dto/PayInfoVo.java
+++ src/main/java/com/tbxx/wpct/dto/PayInfoVo.java
@@ -3,18 +3,26 @@ package com.tbxx.wpct.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PayInfoVo {
-    String villageName;
-    String buildNo;
-    String name;
-    String payStatus;
-    LocalDateTime payBeginTime;
-    LocalDateTime payEndTime;
+    private String villageName;
+    private String buildNo;
+    private String name;
+    private String payStatus;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date payBeginTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date payEndTime;
+
+
+
 
 }
